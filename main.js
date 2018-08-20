e098d5e7b2111b83b45ddccf4d8fc85b02198400
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-08-19 22:01:07 -0500

    circles mini project

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$( document ).ready(()=>{
-    console.log( "ready!" );
-});
+function main() {
+    console.log('ejecuta')
+    $('#circleProject').hide();
+    $('.circlesP').on('click',function(){
+        $('#circleProject').slideToggle(400)
+        startCircles();
+      })
+}
+
+
+$(document).ready(main)
